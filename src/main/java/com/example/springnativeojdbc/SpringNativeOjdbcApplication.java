883fbea2f7c0682af4226851ba5660ddc61fb8fe
Damiano Pittori <damiano.pittori@sisal.it>
2023-03-30 15:19:51 +0200

    first commit

---
+++ src/main/java/com/example/springnativeojdbc/SpringNativeOjdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.springnativeojdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNativeOjdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNativeOjdbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springnativeojdbc/SpringNativeOjdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springnativeojdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNativeOjdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
